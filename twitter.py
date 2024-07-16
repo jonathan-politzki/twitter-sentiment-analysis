a810444c8f5f948e745a419859c86c276b43b165
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2024-07-16 18:26:42 -0500

    it appears X has deprecated previously available API endpoints so I will just do some other stuff instead.

--- twitter.py
+++ twitter.py
@@ -1,11 +1,13 @@
+# libraries
 import re
 import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
 
+# creating a class that will allow us to interact with the Twitter API
 class TwitterClient(object):
     '''
-    Generic Twitter Class for sentiment analysis.
+    Generic Twitter Class for sentiment analysis and tweeting.
     '''
     def __init__(self):
         '''
@@ -28,42 +30,22 @@ class TwitterClient(object):
         except:
             print("Error: Authentication Failed")
 
-    def get_user_tweets(self, username, count=5):
-        tweets = []
+    def tweet(self, message):
+        '''
+        Method to post a tweet with the given message.
+        '''
         try:
-            for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=username).items(count):
-                tweets.append(tweet.text)
-        except tweepy.errors.Forbidden as e:
+            self.api.update_status(message)
+            print("Tweet posted successfully!")
+        except tweepy.TweepError as e:
             print(f"Error: {e}")
-        return tweets
-
-    def analyze_sentiment(self, tweets):
-        sentiment_scores = []
-        for tweet in tweets:
-            analysis = TextBlob(tweet)
-            sentiment_scores.append(analysis.sentiment.polarity)
-        return sentiment_scores
 
 # Create an instance of the TwitterClient class
 client = TwitterClient()
 
-# Specify the username of the user whose tweets you want to analyze
-username = "ITNAmatter"  # Replace with the desired username
-
-# Get the user's tweets
-tweets = client.get_user_tweets(username, count=10)
-
-# Analyze the sentiment of the tweets
-sentiment_scores = client.analyze_sentiment(tweets)
-
-# Print the tweets and their sentiment scores
-for tweet, score in zip(tweets, sentiment_scores):
-    print(f"Tweet: {tweet}")
-    print(f"Sentiment Score: {score}")
-    print("---")
-
-
-
+# Post a tweet
+tweet_text = "Hello, World!"
+client.tweet(tweet_text)
 
 '''
 DEAD CODE
@@ -77,3 +59,22 @@ for tweet in tweets:
     print(tweet.text)
 
 '''
+    """ DEAD CODE FROM THE CLASS - DEPRECATED ENDPOINTS THAT NO LONGER EXIST ON X
+    
+    # creating a method to get the tweets of a specific user
+    def get_user_tweets(self, username, count=5):
+        tweets = []
+        try:
+            for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=username).items(count):
+                tweets.append(tweet.text)
+        except tweepy.errors.Forbidden as e:
+            print(f"Error: {e}")
+        return tweets
+
+    # creating a method to analyze the sentiment of the tweets
+    def analyze_sentiment(self, tweets):
+        sentiment_scores = []
+        for tweet in tweets:
+            analysis = TextBlob(tweet)
+            sentiment_scores.append(analysis.sentiment.polarity)
+        return sentiment_scores"""
